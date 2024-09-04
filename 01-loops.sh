ad9be0f4b06a6862d4a6d71697556745864e5afb
ramesh <rameshpalli@hotmail.com>
2024-09-04 18:50:22 +0200

    variables1 script

---
+++ 01-loops.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+USERID=$(id -u)
+#echo " print the value $USERID"
+CHECKROOT(){
+    if [ $USERID -ne 0 ]
+ then 
+   echo "User not have previlleged access stop execute the script"
+   exit 1
+}
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+    echo -e "$1 is ..$R installation failed $N"
+    else
+    echo -e "$1 is ..$G installation sucess $N"
+    fi
+}
+ CHECKROOT
+fi
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then 
+   echo "package not installed , install the package"
+   dnf install mysql -y
+   VALIDATE $? "Installing mysql"
+    
+else
+      echo "package already installed , nothing to do"
+    
+fi
+
