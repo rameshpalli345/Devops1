dfc5a136b305aae93e71297b2e1c23b58bd56867
ramesh <rameshpalli@hotmail.com>
2024-09-04 14:33:02 +0200

    variables1 script

--- installation.sh
+++ installation.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 USERID=$(id -u)
 #echo " print the value $USERID"
- if [ $USERID -eq 0 ]
+ if [ $USERID -ne 0 ]
  then 
    echo "User not have previlleged access stop execute the script"
    exit 1
+else 
+   echo "User have previlleged access proceed with the script"
 fi
-ls -lr
+dnf list installed mysql
+if [ $? -ne 0 ]
+then 
+   echo "package already installed"
+   exit 1
+else
+   echo "install the package"
+   dnf install mysql -y
+    if [ $? -ne 0]
+    then 
+     echo "package installation failed"
+     exit 1
+    else
+      echo "package installation sucessful"
+    fi
+fi
