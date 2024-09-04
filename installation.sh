8bf9b87ca9969253593557cfdbb5b21ec9d069a6
ramesh <rameshpalli@hotmail.com>
2024-09-04 13:41:42 +0200

    variables1 script

--- installation.sh
+++ installation.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 USERID=$(id -u)
-if [ $USERID -eq 0 ]
+if [ $USERID -ne 0 ]
 then 
-    echo "User have previlleged access then proceed with the script"
+    echo "User not have previlleged access stop execute the script"
+    exit 1
 else
     echo "User not have previlleged access then stop the script"
-    exit 1
+    
 fi
