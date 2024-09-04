4f0dcd85c87c4f1f6aa1e93d28f4a92ed0cbed23
ramesh <rameshpalli@hotmail.com>
2024-09-04 11:52:35 +0200

    variables1 script

---
+++ 01-special_varaiables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "All variables passed to the script: $@"
+echo "Number of variables passed to the script: $#"
+echo "script name : $0"
+echo "PID of current script execution: $$"
+echo "current path of the directory: $PWD"
+echo "home directory of the user : $HOME"
+sleep 100 &
+echo "PID of backgorud process: $1"
