3bde5a8b03d8b3bc6bb2cd8056b301acbd34d0b5
ramesh <rameshpalli@hotmail.com>
2024-09-04 14:43:51 +0200

    variables1 script

--- installation.sh
+++ installation.sh
@@ -9,18 +9,18 @@ else
    echo "User have previlleged access proceed with the script"
 fi
 dnf list install mysql
+
 if [ $? -ne 0 ]
 then 
-   echo "package already installed"
-   exit 1
-else
-   echo "install the package"
+   echo "package not installed , install the package"
    dnf install mysql -y
     if [ $? -ne 0]
     then 
-     echo "package installation failed"
-     exit 1
+     echo "package installation successful"
     else
-      echo "package installation sucessful"
+      echo "package installation faild"
+      exit 1
     fi
+else
+   echo "package already installed , nothing to do"
 fi
