315e84ca6997d868e2da2ed49b9cc3c967ad09c3
ramesh <rameshpalli@hotmail.com>
2024-09-04 11:54:57 +0200

    variables1 script

--- 01-special_varaiables.sh
+++ 01-special_varaiables.sh
@@ -6,4 +6,4 @@ echo "PID of current script execution: $$"
 echo "current path of the directory: $PWD"
 echo "home directory of the user : $HOME"
 sleep 100 &
-echo "PID of backgorud process: $1"
+echo "PID of backgorud process: $!"
