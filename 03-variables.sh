7aab4d84d0949a0cc41a8f019033610b36655c5a
ramesh <rameshpalli@hotmail.com>
2024-09-04 10:37:10 +0200

    variables1 script

---
+++ 03-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "please enter your username::"
+read USERNAME
+echo "your username is:$USERNAME"
+echo "please enter your password::"
+read -s PASSWORD
+echo "your password is:$PASSWORD"
